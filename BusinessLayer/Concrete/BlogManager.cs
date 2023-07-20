b54d690c4d13949b26dd7297e70d996a8319c6f3
ahmettkuurt <65049462+ahmettkuurt@users.noreply.github.com>
2023-07-20 08:28:39 +0300

    Add project files.

---
+++ BusinessLayer/Concrete/BlogManager.cs
@@ -0,0 +1,55 @@
+﻿using BusinessLayer.Abstract;
+using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.Concrete
+{
+	public class BlogManager : IBlogService
+	{
+		IBlogDal _blogDal;
+
+		public BlogManager(IBlogDal blogDal)
+		{
+			_blogDal = blogDal;
+		}
+
+		public void AddBlog(Blog blog)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void DeleteBlog(Blog blog)
+		{
+			throw new NotImplementedException();
+		}
+
+        public List<Blog> GetBLogListWithCategory()
+        {
+            return _blogDal.GetListWithCategory();
+        }
+
+        public Blog GetById(int id)
+		{
+			throw new NotImplementedException();
+		}
+
+		public List<Blog> GetList()
+		{
+			return _blogDal.GetListAll();
+		}
+		public List<Blog> GetBlogById(int id)
+		{
+			return _blogDal.GetListAll(x=> x.BlogID == id);
+		}
+
+		public void UpdateBlog(Blog blog)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
